490a5944344529fbff3fccbc1d182ff8b0d82784
AlexisLELEU <alexis.leleu@hetic.net>
2017-06-06 11:33:51 +0200

    choose your extension

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def errorStr(typeError):
     print('\033[1;31;40m')
     print(typeError)
     print(' _____   _____    _____    _____   _____ ')
-    print("| ____| |  _  \  |  _  \  /  _  \ |  _  \ ")
+    print('| ____| |  _  \  |  _  \  /  _  \ |  _  \ ')
     print('| |__   | |_| |  | |_| |  | | | | | |_| |')
     print('|  __|  |  _  /  |  _  /  | | | | |  _  /')
     print('| |___  | | \ \  | | \ \  | |_| | | | \ \ ')
@@ -27,18 +27,42 @@ def createFolder(name):
     os.mkdir(name)
 
 
-def createFolderDefault():
+def createFolderDefault(ext):
     createFolder('img-content')
     createFolder('img-layout')
     createFolder('js')
     createFolder('styles')
     os.chdir(path + '/app/styles')
-    createFile('styles.scss', os.O_WRONLY | os.O_CREAT)
-    createFile('styles.css', os.O_RDWR | os.O_CREAT)
+
     os.chdir(path + '/app/js')
     createFile('app.js', os.O_RDWR | os.O_CREAT)
     os.chdir(path + '/app')
-    createFile('index.html', os.O_RDWR | os.O_CREAT)
+    createFile('index.' + ext, os.O_RDWR | os.O_CREAT)
+
+    idx = os.open('index.' + ext, os.O_RDWR | os.O_CREAT)
+    os.write(idx,bytes(
+        '<!DOCTYPE html>\n<html>\n  <head>\n '
+        '<meta charset="utf-8">\n '
+        '<link rel="stylesheet" href="styles/styles.css">\n '
+        '<title></title>\n  </head>\n  <body>\n\n\n  </body>\n</html>'))
+    os.close(idx)
+
+
+def editStyle(text):
+    css = os.open(text, os.O_RDWR | os.O_CREAT)
+    os.write(css,bytes('@charset "UTF-8";\n\n\n'
+                       '/*/////////////////////////////////////////////////////////////////////////////'
+                       '\n'
+                       '\n'
+                       '                                    Commons \n'
+                       '\n'
+                       '\n'
+                       '/////////////////////////////////////////////////////////////////////////////*/\n'
+                       '\n'
+                       ' *{\n '
+                       'margin: 0;\n padding: 0;\n}\n*,*:before,*:after {\n   box-sizing:border-box\n}'))
+    os.close(css)
+
 
 print('\033[1;32;40m')
 print('PathLab - Simply generator Files/Folders for a fast Workflow \n\n ' )
@@ -47,17 +71,24 @@ print('Thanks you to use this script, its made by Jonathan IBOR student @HETIC \
 
 start = str(input('Ready to start ? (0/1) : '))
 sass = str(input('Want to launch SASS Watcher ? (0/1) : '))
+extensionFile = str(input('Want to launch PHP or HTML ? 0->php // 1->html: '))
+
 if start == '0':
     if not (P.exists(path + '/app')):
         createFolder('app')
         os.chdir(app)
-        createFolderDefault()
+        if extensionFile == '0':
+            createFolderDefault('php')
+        else:
+            createFolderDefault('html')
         if sass == '0' :
             os.chdir(path + '/app/styles')
+            editStyle('styles.scss')
             os.system('sass --watch styles.scss:styles.css')
+
         else:
             os.chdir(path + '/app/styles')
-            os.remove('styles.scss')
+            editStyle('styles.css')
 
     else:
         errorStr('Folders already exist')
